a9d9ccfc36aafa6999c3cc50aed4bd169a3285da
kim young joong <116442812+Yanasna@users.noreply.github.com>
2025-07-03 14:47:35 +0900

    feat : update file

--- ConfuseAge_KATSU/Agriculture.cpp
+++ ConfuseAge_KATSU/Agriculture.cpp
@@ -14,127 +14,209 @@ static int month = 1;
 static int P_income = 0;
 
 void Turn();
+void Hatakeyama();
 
 void agriculture() {;
-    try {
-        //가문의 성 정보 불러오기
-        ifstream HouseFile("../../ConfuseAge_KATSU/HouseInfo.json");
-        if (!HouseFile.is_open()) {
-            cerr << "파일 열기 실패!" << endl;
-            return;
+try {
+    //가문의 성 정보 불러오기
+    ifstream HouseFile("../../ConfuseAge_KATSU/HouseInfo.json");
+    if (!HouseFile.is_open()) {
+        cerr << "파일 열기 실패!" << endl;
+        return;
+    }
+    stringstream buffer1;
+    buffer1 << HouseFile.rdbuf();  // 전체 파일을 문자열로 읽기
+    json HouseData = json::parse(buffer1.str());  // HouseInfo문자열을 파싱
+
+
+    //성 데이터 불러오기
+    ifstream file("../../ConfuseAge_KATSU/CastleData.json");
+    if (!file.is_open()) {
+        cerr << "파일 열기 실패!" << endl;
+        return;
+    }
+
+    stringstream buffer2;
+    buffer2 << file.rdbuf();  // 전체 파일을 문자열로 읽기
+    json CastleData = json::parse(buffer2.str());  // CastleData.json 문자열을 파싱
+
+    //장군 정보 불러오기
+    ifstream GeneralFile("../../ConfuseAge_KATSU/GeneralInfo.json");
+    if (!GeneralFile.is_open()) {
+        cerr << "파일 열기 실패!" << endl;
+        return;
+    }
+    stringstream buffer3;
+    buffer3 << GeneralFile.rdbuf();  // 전체 파일을 문자열로 읽기
+    json GeneralData = json::parse(buffer3.str());  // 문자열을 파싱
+
+
+    cout << "개발할 성을 선택해 주세요!" << endl;
+    int position = 0;
+
+    bool running = true;
+    int count = 0;
+
+    for (const auto& entry : HouseData) {
+        if (entry["House"] == "Hatakeyama") {
+            for (const auto& castle : entry["Castle_List"]) {
+                for (const auto& castleEntry : CastleData) {
+                    if (castle == castleEntry["castle_name"]) {
+                        cout << "성 이름 : " << castleEntry["castle_name"] << " Castle  " << "  |  ";
+                        cout << "농업 최대치 : " << castleEntry["Max_agriculture"] << "  ||  현재 농업 진행 : " << castleEntry["Early_agriculture"]
+                            << (position == count ? "<-" : "  ") << "    " << endl;
+                        count++;
+                    }
+                }
+                
+            }
         }
-        stringstream buffer1;
-        buffer1 << HouseFile.rdbuf();  // 전체 파일을 문자열로 읽기
-        json HouseData = json::parse(buffer1.str());  // HouseInfo문자열을 파싱
+    }
 
-
-        //성 데이터 불러오기
-        ifstream file("../../ConfuseAge_KATSU/EarlyCastleData.json");
-        if (!file.is_open()) {
-            cerr << "파일 열기 실패!" << endl;
-            return;
+    while (running) {
+        int country_key = _getch();
+        if (country_key == 224) {
+            int arrow = _getch();
+            if (arrow == 75) { // ←
+                if (position > 0)
+                    position--;
+            }
+            else if (arrow == 77) { // →
+                if (position < count)
+                    position++;
+            }
         }
+        else if (country_key == 13) { // Enter 키
+            system("cls");
+            json Element = CastleData[position];
+            string Castle_Name = Element["castle_name"];
+            cout << Castle_Name << "성의 농업을 담당할 사람을 임명해주세요!" << endl;
 
-        stringstream buffer2;
-        buffer2 << file.rdbuf();  // 전체 파일을 문자열로 읽기
-        json data = json::parse(buffer2.str());  // EarlyCastleData.json 문자열을 파싱
-
-        //장군 정보 불러오기
-        ifstream GeneralFile("../../ConfuseAge_KATSU/GeneralInfo.json");
-        if (!GeneralFile.is_open()) {
-            cerr << "파일 열기 실패!" << endl;
-            return;
-        }
-        stringstream buffer3;
-        buffer3 << GeneralFile.rdbuf();  // 전체 파일을 문자열로 읽기
-        json GeneralData = json::parse(buffer3.str());  // 문자열을 파싱
-
-
-        cout << "개발할 성을 선택해 주세요!" << endl;
-        int position = 0;
-        
-        bool running = true;
-        int count = 0;
-        
-        for (const auto& entry : HouseData) {
-            if (entry["House"] == "Hatakeyama") {
-                count = entry["Castle_List"].size() -1;
-                for (const auto& castle : entry["Castle_List"]) {
-                    for (const auto& castleEntry : data) {
-                        if (castle == castleEntry["castle_name"]) {
-                            cout << "성 이름 : " << castleEntry["castle_name"] << " Castle  " << "  |  ";
-                            cout << "농업 최대치 : " << castleEntry["Max_agriculture"] << "  ||  현재 농업 진행 : " << castleEntry["Early_agriculture"]
-                                << (position == count ? "<-" : "  ") << "    " << endl;
+            bool general_running = true;
+            int general_count = 0;
+            int general_position = 0;
+            for (const auto& entry : HouseData) {
+                if (entry["House"] == "Hatakeyama") {
+                    for (const auto& general : entry["General_List"]) {
+                        for (const auto& GeneralEntry : GeneralData) {
+                            if (general == GeneralEntry["General_Name"]) {
+                                cout << "장군 이름 : " << GeneralEntry["General_Name"] << "  |  ";
+                                cout << "무력 : " << GeneralEntry["Attack"] << "  ||  지략 : " << GeneralEntry["resourceful"]
+                                    << (general_position == general_count ? "<-" : "  ") << "    " << endl;
+                                general_count++;    
+                            }
                         }
+                       
                     }
                 }
             }
-        }
+            while (general_running) {
+                int general_key = _getch();
+                if (general_key == 224) {
+                    int general_arrow = _getch();
+                    if (general_arrow == 75) { // ←
+                        if (general_position > 0)
+                            general_position--;
+                    }
+                    else if (general_arrow == 77) { // →
+                        if (general_position < general_count)
+                            general_position++;
+                    }
+                }
+                else if (general_key == 13)
+                {
+                    char agriculture_num;
 
-        while (running) {
-            int country_key = _getch();
-            if (country_key == 224) {
-                int arrow = _getch();
-                if (arrow == 75) { // ←
-                    if (position > 0)
-                        position--;
+                    system("cls");
+                    cout << "농업 개발 시에는 자금 1000원이 소모됩니다!" << endl;
+                    cout << "개발 하시겠습니까?" << endl;
+                    cout << "예 -> Y , 아니오 -> N" << endl;
+                    cin >> agriculture_num;
+
+                    if (agriculture_num == 'Y' || agriculture_num == 'y') {
+                        money -= 1000;
+                        cout << "농업 - 개발중" << endl;
+                        for (auto& castle : CastleData) {
+                            if (castle["castle_name"] == Castle_Name) {
+                                castle["Early_agriculture"] += 10; // ✅ 여기서 수정!
+                            }
+                        }
+                        ofstream fileOut("EarlyCastleData.json");
+                        if (!fileOut.is_open()) {
+                            cerr << "파일 저장 실패!" << endl;
+                        }
+                        fileOut << CastleData.dump(4); //
+                        fileOut.close();
+
+                        for (int i = 0; i < 5; i++)
+                        {
+                            Sleep(500);
+                            cout << "=";
+                        }
+                        for (int i = 0; i < 5; i++)
+                        {
+                            Sleep(200);
+                            cout << "=";
+                        }
+                        for (int j = 0; j < 10; j++) {
+                            Sleep(50);
+                            cout << "=";
+                        }
+                        cout << endl;
+                        cout << "농업 개발 완료!" << endl;
+                        Sleep(1000);
+                        Turn();
+                        Hatakeyama();
+                    }
+                    else {
+                        Hatakeyama();
+                    }
                 }
-                else if (arrow == 77) { // →
-                    if (position < count)
-                        position++;
-                }
-            }
-            else if (country_key == 13) { // Enter 키
                 system("cls");
-                json Element = data[position];
-                string Castle_Name = Element["castlename"];
-                cout << Castle_Name << "성의 농업을 담당할 사람을 임명해주세요!";
-
                 int general_count = 0;
-                int general_position = 0;
+                cout << Castle_Name << "성의 농업을 담당할 사람을 임명해주세요!" << endl;
                 for (const auto& entry : HouseData) {
                     if (entry["House"] == "Hatakeyama") {
-                        general_count = entry["General_List"].size() - 1;
                         for (const auto& general : entry["General_List"]) {
-                            for (const auto& GeneralEntry : data) {
+                            for (const auto& GeneralEntry : GeneralData) {
                                 if (general == GeneralEntry["General_Name"]) {
                                     cout << "장군 이름 : " << GeneralEntry["General_Name"] << "  |  ";
                                     cout << "무력 : " << GeneralEntry["Attack"] << "  ||  지략 : " << GeneralEntry["resourceful"]
                                         << (general_position == general_count ? "<-" : "  ") << "    " << endl;
+                                    general_count++;
+                                }
+                            }
+                            
+                        }
+                    }
+                }
+            }
+
+                system("cls");
+
+                cout << "농업을 증진할 성을 선택하세요!" << endl;
+                for (const auto& entry : HouseData) {
+                    if (entry["House"] == "Hatakeyama") {
+                        count = entry["Castle_List"].size() - 1;
+                        for (const auto& castle : entry["Castle_List"]) {
+                            for (const auto& castleEntry : CastleData) {
+                                if (castle == castleEntry["castle_name"]) {
+                                    cout << "성 이름 : " << castleEntry["castle_name"] << " Castle  " << "  |  ";
+                                    cout << "농업 최대치 : " << castleEntry["Max_agriculture"] << "  ||  현재 농업 진행 : " << castleEntry["Early_agriculture"]
+                                        << (position == count ? "<- " : "  ") << "    " << endl;
                                 }
                             }
                         }
                     }
-                }
-
-
-            }
-            system("cls");
-            
-            cout << "농업을 증진할 성을 선택하세요!" << endl;
-            for (const auto& entry : HouseData) {
-                if (entry["House"] == "Hatakeyama") {
-                    count = entry["Castle_List"].size() - 1;
-                    for (const auto& castle : entry["Castle_List"]) {
-                        for (const auto& castleEntry : data) {
-                            if (castle == castleEntry["castle_name"]) {
-                                cout << "성 이름 : " << castleEntry["castle_name"] << " Castle  " << "  |  ";
-                                cout << "농업 최대치 : " << castleEntry["Max_agriculture"] << "  ||  현재 농업 진행 : " << castleEntry["Early_agriculture"]
-                                    << (position == count ? "<- " : "  ") << "    " << endl;
-                            }
-                        }
-                    }
-                }
             }
         }
-        Turn();
-        Sleep(3000);
+        }
     }
     catch (const json::parse_error& e) {
         cerr << "JSON 파싱 에러 발생: " << e.what() << endl;
         Sleep(3000);
     }
-       
+
     Sleep(3000);
 }
 
@@ -154,7 +236,7 @@ void Income() {
 
 
         //성 데이터 불러오기
-        ifstream file("../../ConfuseAge_KATSU/EarlyCastleData.json");
+        ifstream file("../../ConfuseAge_KATSU/CastleData.json");
         if (!file.is_open()) {
             cerr << "파일 열기 실패!" << endl;
             return;
@@ -162,14 +244,14 @@ void Income() {
 
         stringstream buffer2;
         buffer2 << file.rdbuf();  // 전체 파일을 문자열로 읽기
-        json data = json::parse(buffer2.str());  // 문자열을 파싱
+        json CastleData = json::parse(buffer2.str());  // 문자열을 파싱
 
 
         P_income = 0;
         for (const auto& entry : HouseData) {
             if (entry["House"] == "Hatakeyama") {
                 for (const auto& castle : entry["Castle_List"]) {
-                    for (const auto& castleEntry : data) {
+                    for (const auto& castleEntry : CastleData) {
                         if (castle == castleEntry["castle_name"]) {
                             P_income += castleEntry["Early_commerce"].get<int>() * (castleEntry["Population"].get<int>()/ 10000);
                         }
@@ -179,8 +261,7 @@ void Income() {
         }
         cout << " 수입 : " << P_income << endl;
         cout << " 자금 : " << money << endl;
-    }
-        catch (const json::parse_error& e) {
+    }catch (const json::parse_error& e) {
             cerr << "JSON 파싱 에러 발생: " << e.what() << endl;
             Sleep(3000);
         }
