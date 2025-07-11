d8ea745de9a2898d8bcf62e09837b04c6457245e
kim young joong <116442812+Yanasna@users.noreply.github.com>
2025-07-11 13:09:27 +0900

    feat : update file

--- ConfuseAge_KATSU/GameOver.cpp
+++ ConfuseAge_KATSU/GameOver.cpp
@@ -6,11 +6,33 @@ using namespace std;
 
 void GameOver_Nomoney() {
 	system("cls");
-	string text = "이런.. 돈을 다 소모하셨군요,,";
+	Sleep(2000);
+
+	string text = ". . . . . . .";
+	string text1 = "이런.. 돈을 다 소모하셨군요,,";
+	string text2 = "파산하셨습니다....";
+	string text3 = "파 산 엔 딩!";
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
 	for (char ch : text) {
+		cout << ch;
+		Sleep(100);
+	}
+	cout << endl;
+	for (char ch : text1) {
 		cout << ch ;
 		Sleep(100); 
 	}
-	cout << "파산 엔딩..!" << endl;
+	cout << endl;
+	for (char ch : text2) {
+		cout << ch;
+		Sleep(100);
+	}
+	cout << endl;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+	for (char ch : text3) {
+		cout << ch;
+		Sleep(100);
+	}
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 1);
 	exit(0);
 }
